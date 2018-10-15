c6c8271e0603fd7dda6f6b3182a6cc2e3df170fe
Manish Saini <Manishsaini698>
2018-10-15 21:14:57 +0530

    getting started

--- wallet/__init__.py
+++ wallet/__init__.py
@@ -8,5 +8,7 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('login', '/login')
+    config.add_route('signup', '/signup')
     config.scan()
     return config.make_wsgi_app()
--- wallet/views.py
+++ wallet/views.py
@@ -1,6 +1,14 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-def my_view(request):
+@view_config(route_name='home', renderer='templates/index.jinja2')
+def home(request):
+    return {'project': 'wallet'}
+
+@view_config(route_name='login', renderer='templates/login.jinja2')
+def login(request):
+    return {'project': 'wallet'}
+
+@view_config(route_name='signup', renderer='templates/signup.jinja2')
+def signup(request):
     return {'project': 'wallet'}
